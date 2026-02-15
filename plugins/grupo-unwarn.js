1fb54835984b4ef3780907c806314e1f849b81d4
gabrie-cell <doxeosjr@gmail.com>
2026-02-15 18:42:20 -0500

    Actualizar grupo-unwarn.js

--- plugins/grupo-unwarn.js
+++ plugins/grupo-unwarn.js
@@ -14,6 +14,7 @@ const handler = async (m, {conn, text, command, usedPrefix}) => {
 };
 handler.command = ['delwarn', 'unwarn']
 handler.group = true;
+hamdler.tag = ['grupos']
 handler.admin = true;
 handler.botAdmin = true;
 
